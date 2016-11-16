8adbc56e3b5de1a1c5c9f2caa9f2bc0ce12f31e6
Kevin <kevin@MacBook-Pro-de-Kevin.local>
2016-11-16 15:29:43 +0100

    Update

--- server.js
+++ server.js
@@ -5,17 +5,15 @@ var app = express();
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
 
 // make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/www'));
 
 // set the home page route
 app.get('/', function(req, res) {
 
 	// ejs render automatically looks in the views folder
-	res.render('index');
+	res.send('hello kvn');
 });
 
 app.listen(port, function() {
